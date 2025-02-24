fc921d199c5868e9cdcac64ddc4ffbe10e138702
joefrazier69 <baris21221124@gmail.com>
2025-02-24 14:06:12 +0300

    Update PhysicsList.cc

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,9 +1,8 @@
 #include "PhysicsList.hh"
-#include "G4EmStandardPhysics_option3.hh"  // Faster EM physics
-// Alternatively, you can use G4EmLivermorePhysics for low-energy optimizations
+#include "G4EmStandardPhysics_option3.hh"   
 
 PhysicsList::PhysicsList() {
-    // Use G4EmStandardPhysics_option3 for faster simulations
+    
     RegisterPhysics(new G4EmStandardPhysics_option3());
 }
 
