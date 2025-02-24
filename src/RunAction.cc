e65c30c174162577195f97b2f0463d55cbf1662b
joefrazier69 <baris21221124@gmail.com>
2025-02-24 14:06:57 +0300

    Update RunAction.cc

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,7 +1,7 @@
 #include "RunAction.hh"
 #include "G4Run.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4UnitsTable.hh"  // Include for G4BestUnit
+#include "G4UnitsTable.hh"   
 
 RunAction::RunAction(SteppingAction* steppingAction) : fSteppingAction(steppingAction) {}
 
@@ -14,12 +14,12 @@ void RunAction::BeginOfRunAction(const G4Run* run) {
 void RunAction::EndOfRunAction(const G4Run* run) {
     G4cout << "### Run " << run->GetRunID() << " ended." << G4endl;
 
-    // Calculate total energy deposited and absorbed dose
+     
     G4double totalEnergyDeposited = fSteppingAction->GetTotalEnergyDeposited();
-    G4double mass = 2.329 * kg;  // Mass of silicon block
+    G4double mass = 2.329 * kg;   
     G4double dose = totalEnergyDeposited / mass;
 
-    // Print results
+     
     G4cout << "Total Energy Deposited: " << G4BestUnit(totalEnergyDeposited, "Energy") << G4endl;
     G4cout << "Absorbed Dose: " << G4BestUnit(dose, "Dose") << G4endl;
 }
