683eff14a28d6b0c832c240c858cc2f6cb2b1937
joefrazier69 <baris21221124@gmail.com>
2025-04-28 23:33:13 +0300

    Update PhysicsList.hh

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -1,12 +1,15 @@
-#ifndef PhysicsList_h
-#define PhysicsList_h 1
+#ifndef PHYSICSLIST_HH
+#define PHYSICSLIST_HH
 
 #include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics_option4.hh"  // Electromagnetic physics
+#include "G4HadronPhysicsFTFP_BERT.hh"    // Hadronic interactions
+#include "G4IonPhysics.hh"                // Ion physics
 
 class PhysicsList : public G4VModularPhysicsList {
 public:
     PhysicsList();
-    virtual ~PhysicsList();
+    ~PhysicsList();
 };
 
 #endif
