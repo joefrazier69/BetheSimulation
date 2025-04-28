d66d18ad72ed0d615def076b26d77b32e8e96cc8
joefrazier69 <baris21221124@gmail.com>
2025-04-28 23:27:22 +0300

    Update PhysicsList.cc

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,9 +1,12 @@
 #include "PhysicsList.hh"
-#include "G4EmStandardPhysics_option3.hh"   
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4HadronPhysicsFTFP_BERT.hh"
+#include "G4IonPhysics.hh"
 
 PhysicsList::PhysicsList() {
-    
-    RegisterPhysics(new G4EmStandardPhysics_option3());
+    RegisterPhysics(new G4EmStandardPhysics_option4());  // Electromagnetic interactions
+    RegisterPhysics(new G4HadronPhysicsFTFP_BERT());    // Hadronic interactions
+    RegisterPhysics(new G4IonPhysics());               // Ion physics 
 }
 
 PhysicsList::~PhysicsList() {}
